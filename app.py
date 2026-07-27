3aa3c0a6190a4e49e5352c6d45762d79ac320eaf
yashbora18 <yashbora0801@gmail.com>
2026-07-27 12:43:44 +0530

    Prepare project for Koyeb deployment

--- app.py
+++ app.py
@@ -100,5 +100,8 @@ def search():
     return render_template("index.html", students=students)
 
 # Run locally
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
